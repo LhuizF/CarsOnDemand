846c38c37838bc236b9d4e4ba66c66c569d602d6
Luiz Henrique Ferreira <luizhff@gmail.com>
2021-05-01 21:20:34 -0300

    new commit

--- index.js
+++
@@ -1,9 +0,0 @@
-const cars = document.getElementsByClassName("cars")
-function start() {
-    for (let i = 0; i < cars.length; i++) {
-        console.log(cars[i])
-        if (i < cars.length) {
-        }
-    }
-}
-start()
