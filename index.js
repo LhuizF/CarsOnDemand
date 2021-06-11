a7fd6406eb91c47d6a52c5c3ba99d0164dcd7f40
Luiz Henrique Ferreira <luizhff@gmail.com>
2021-06-10 23:15:01 -0300

    vai

--- index.js
+++ index.js
@@ -6,40 +6,33 @@ const outputNome = document.getElementById("name");
 
 let carNumber = 0;
 let autoTime;
-let cars = [
+
+const cars = [
     {
-        "img": "https://static.wixstatic.com/media/0a1729_89ab724db89a4de694b52ae7b58035a6~mv2.png/v1/fill/w_408,h_253,al_c,q_80,usm_0.66_1.00_0.01/0a1729_89ab724db89a4de694b52ae7b58035a6~mv2.png",
-        "nome": "Onix joy 1.0 2018",
-        "id": "car1"
+        "name": "Renault Duster",
+        "img" : "https://www.minasfrance.com.br/uploads/products/versions/colors/renault-duster-zen-cvt-preto-nacre.png"
+    },
+
+    {
+        "name": "Chevrolet Onix",
+        "img" : "https://static.wixstatic.com/media/0a1729_89ab724db89a4de694b52ae7b58035a6~mv2.png/v1/fill/w_408,h_253,al_c,q_80,usm_0.66_1.00_0.01/0a1729_89ab724db89a4de694b52ae7b58035a6~mv2.png"
+    },
+
+    {
+        "name": "Hyundai HB20 ",
+        "img" : "https://static.wixstatic.com/media/ecaeac_67b6d3ea7e9143d2a1013052f7983f41~mv2.png/v1/fill/w_640,h_289,al_c/ecaeac_67b6d3ea7e9143d2a1013052f7983f41~mv2.png"
+    },
+
+    {
+        "name": "Volkswagen Amarok",
+        "img" : "https://static.virgola.com.br/images/fd4ae2b6759e8eca4bd706c0b4224c0540dcf6e1f52b054f9d2ecb22cac8009b.png"
     },
     {
-        "img": "https://static.wixstatic.com/media/0a1729_124c40d409b5473f9d89f968a0c4c7eb~mv2.png/v1/crop/x_8,y_0,w_760,h_419/fill/w_450,h_248,al_c,q_85,usm_0.66_1.00_0.01/BrancoGlacier-1.webp",
-        "nome": "Sandero Authetique 1.0 2018",
-        "id": "car2"
-    },
-    {
-        "img": "https://static.wixstatic.com/media/0a1729_154cf7d34ad749b2b80567a05e44041f~mv2.png/v1/crop/x_0,y_99,w_440,h_242/fill/w_450,h_248,al_c,lg_1,q_85/voyage440.webp",
-        "nome": "Voyage 1.6 trend line 2018",
-        "id": "car3"
-    },
-    {
-        "img": "https://static.wixstatic.com/media/0a1729_40ed1f4746904e1596ffd1fdbf45cc02~mv2.png/v1/crop/x_0,y_64,w_1400,h_772/fill/w_423,h_232,al_c,q_85,usm_0.66_1.00_0.01/69573_1522076091.webp",
-        "nome": "Renault Logan 1.0 Authetique Prata 2018",
-        "id": "car4"
-    },
-    {
-        "img": ".././Img/audi a8.png",
-        "nome": "Audi a8",
-        "id": "car5"
+        "name": "Renault Sandero",
+        "img" : "https://static.wixstatic.com/media/0a1729_124c40d409b5473f9d89f968a0c4c7eb~mv2.png/v1/crop/x_8,y_0,w_760,h_419/fill/w_450,h_248,al_c,q_85,usm_0.66_1.00_0.01/BrancoGlacier-1.webp",
     }
 ]
 
-// async function Api(){
-//     const apiCars = (".././ListCars.json");
-//     const Retorn = await fetch(apiCars);
-//     cars = await Retorn.json();
-//     timeInterval();
-// };
 
 
 function exibir(ord) {
@@ -57,12 +50,12 @@ function display(){
     let imgNow = cars[carNumber].img
     outputImg.src = imgNow
 
-    let nameNow = cars[carNumber].nome
+    let nameNow = cars[carNumber].name
     outputNome.textContent = nameNow
 };
 
 function timeInterval(){
-    autoTime = setInterval(function(){
+    autoTime = setInterval(()=>{
         exibir("next")
     }, 2000)
     display();
@@ -81,5 +74,4 @@ nextbtn.addEventListener("mouseout", timeInterval);
 mainCar.addEventListener("mouseover", stopTime);
 mainCar.addEventListener("mouseout", timeInterval);
 
-// Api();
 timeInterval()
--- scriptcars.js
+++ scriptcars.js
@@ -1,13 +1,90 @@
-const btnCars = document.querySelectorAll(".btn-info"); // array dos buttons
+async function getApi(){
+    const url = "https://jsonkeeper.com/b/Y1YL"
+    const api = await fetch(".././ListCars.json");
+    const carsApi = await api.json();
+    createElement(carsApi)
+}
 
-btnCars.forEach(function (car){// seleciona todos os buttons individualmente  
-    car.addEventListener("click", () =>{ // adiciona um evento de click
-        const infoCar = car.nextElementSibling.classList; //seleciona o primeiro elemento em relação ao button
-        if(!infoCar.contains("open")){// se o elemento selecionado NÃO tiver a class "open" ele adiciona a class "open" e altera a arrow em 180 graus 
-            infoCar.add("open"); car.firstChild.style.transform = "rotate(180deg)"; 
-            
-        }else{// remove a class "open" e reverte a alteração na arrow
-            infoCar.remove("open"); car.firstChild.style.transform = "rotate(0deg)";
-        };
+function createElement(carsApi){
+    
+    for(let i = 0; i < carsApi.length; i++){
+        const imgCar = carsApi[i].img;
+        const nameCar = carsApi[i].name;
+        const nameMarca = carsApi[i].marca
+        const yearCar = carsApi[i].ano
+        const carSelected = carsApi[i].details;
+        const carComplement = carsApi[i].complement
+
+        let space = " "
+        const title = `${nameCar} ${nameMarca}`
+        if(title.length < 15){space = "</br>"};
+
+        const car = 
+        `<div class="car">
+            <img src="${imgCar} " alt="">
+            <h2>${title}${space}${yearCar}</h2>
+            <hr>
+            <h3>Informações</h3>
+            <div class = "main_info"></div>
+            <div class = "btn-container">
+                <button class="btn-info"><i class="material-icons">expand_more</i>Mais detalhes</button>
+                <div class="cars-details"></div>
+                <button class="btn-reserve">RESERVAR AGORA</button>
+            </div>
+        </div>`
+        document.querySelector(".cars-container").innerHTML += car;
+
+        const listInfo = document.createElement("div");
+        const divInfo = document.querySelectorAll(".main_info");
+        const key = Object.keys(carSelected);
+
+        for(let i in key){
+            const keyName = key[i][0].toUpperCase() + key[i].slice(1);
+            const keyInfo = carSelected[key[i]][0].toUpperCase() + carSelected[key[i]].slice(1);
+            const nameInfo = `${keyName}: ${keyInfo}`;
+            const span = document.createElement("span");
+            span.innerHTML += nameInfo;
+            listInfo.append(span);
+        }
+
+        const listDetails = document.createElement("div")
+        const keyComplement = Object.keys(carComplement)
+        const divDetails = document.querySelectorAll(".cars-details");
+
+        for(let i in keyComplement){
+            const moreDetails = keyComplement[i];
+            const divDetailsInfo = document.createElement("div");
+            divDetailsInfo.classList.add("details-info")
+
+            const span = document.createElement("span");
+            const icon = document.createElement("img");
+
+            divDetailsInfo.append(icon, span)
+            const materialIcons = carComplement[keyComplement[i]]
+        
+            icon.src = materialIcons;
+            span.innerText += moreDetails;
+            listDetails.appendChild(divDetailsInfo)
+        }
+        
+        divInfo[i].appendChild(listInfo)
+        divDetails[i].appendChild(listDetails)
+
+    }
+
+    const btnCars = document.querySelectorAll(".btn-info");         // array dos buttons
+
+    btnCars.forEach(function (car){                                 // seleciona todos os buttons individualmente  
+        car.addEventListener("click", () =>{                        // adiciona um evento de click
+            const infoCar = car.nextElementSibling.classList;       //seleciona o primeiro elemento em relação ao button
+            if(!infoCar.contains("open")){                          // se o elemento selecionado NÃO tiver a class "open" ele adiciona a class "open" e altera a arrow em 180 graus 
+                infoCar.add("open"); car.firstChild.style.transform = "rotate(180deg)"; 
+                
+            }else{                                                  // remove a class "open" e reverte a alteração na arrow
+                infoCar.remove("open"); car.firstChild.style.transform = "rotate(0deg)";
+            };
+        })
     })
-})
+}
+
+getApi()
